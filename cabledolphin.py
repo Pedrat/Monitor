38db5988842383d651ad53fffe1dd15655fb2a9d
Pedrat <pedrat@localhost.localdomain>
2018-04-10 08:08:14 +0100

    dsa

--- cabledolphin.py
+++ cabledolphin.py
@@ -7,10 +7,14 @@ os.system("clear")
 capture = pyshark.LiveCapture(interface='eth0')
 for packet in capture.sniff_continuously():
     try:
-        print(packet["TCP"].dstport)#.dst)
+        #print(packet["TCP"].dstport)#.dst)
+        if packet["TCP"].dstport == "80":
+            print(colored("HTTP Response","cyan"))
+        elif packet["TCP"].dstport == "443":
+            print (colored('HTTPS Response',"blue"))
+        elif packet["TCP"].srcport == "80":
+            print (colored('HTTP Request',"yellow"))
+        elif packet["TCP"].srcport == "443":
+            print (colored('HTTPS Request',"red"))
     except:
         pass
-    if "HTTP" in packet:
-        print (colored('Just arrived',"white"))
-    if "HTTPS" in packet:
-        print(colored("Just arrived",'red'))
