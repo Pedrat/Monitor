a8659bfee39b013e9533872f57245c53c96dc62a
Pedrat <pedrat@localhost.localdomain>
2018-04-10 10:31:14 +0100

    sad

--- cabledolphin.py
+++ cabledolphin.py
@@ -1,9 +1,22 @@
-import os,sys,scapy,pyshark,cabledolphinbrain
+import os,sys,scapy,pyshark
+import cabledolphinbrain as cdb
 from time import sleep
 from threading import Thread as th
 from termcolor import colored,cprint
-os.system("clear")
+#os.system("clear")
+obj=cdb.CAPTURE()
+obj.thread()
+while obj.valida == True:
+    #sleep(2)
+    os.system("clear")
+    print(obj.HTTP,"\n",obj.HTTPS)
 
 
 
-print(cabledolphinbrain.HTTP,"\n",cabledolphinbrain.HTTPS)
+def input():
+    #while 1:
+    a=getch()
+        #if a == "^[":
+        #    valida=False
+        #    #sys.exit(0)
+        #    print(colored(a,'red'),"CARALHO")
--- cabledolphinbrain.py
+++ cabledolphinbrain.py
@@ -1,26 +1,39 @@
 import os,sys,pyshark
+from getch import getch
 from time import sleep
 from termcolor import colored,cprint
+from threading import Thread as th
 
-HTTP=[0,0]
-HTTPS=[0,0]
 
-def incrsrc(port):
-    if port == "80":
-        HTTP[0]+=1
-    elif port == "443":
-        HTTPS[0]+=1
-def incrdst(port):
-    if port == "80":
-        HTTP[1]+=1
-    elif port =="443":
-        HTTPS[1]+=1
 
-def capture():
-    capture = pyshark.LiveCapture(interface='eth0')
-        for packet in capture.sniff_continuously():
-            try:
-                incrsrc(packet["TCP"].srcport)
-                incrdst(packer["TCP"].dstport)
-            except:
-                pass
+class CAPTURE:
+    def __init__(self):
+        self.HTTP=[0,0]
+        self.HTTPS=[0,0]
+        self.valida=True
+    def incrsrc(self,port):
+        if port == "80":
+            self.HTTP[0]+=1
+        elif port == "443":
+            self.HTTPS[0]+=1
+    def incrdst(self,port):
+        if port == "80":
+            self.HTTP[1]+=1
+        elif port =="443":
+            self.HTTPS[1]+=1
+
+    def capture(self):
+        while self.valida == True:
+            capture = pyshark.LiveCapture(interface='eth0')
+            for packet in capture.sniff_continuously():
+                try:
+                    self.incrsrc(packet["TCP"].srcport)
+                    self.incrdst(packet["TCP"].dstport)
+                except:
+                    pass
+
+
+    def thread(self):
+        thr=th(target=self.capture)
+        thr.daemon=True
+        thr.start()
