a4c8120073afe211a0e7a4a6baa51f240d3393dc
Pedrat <pedrat@localhost.localdomain>
2018-04-10 08:46:04 +0100

    sad

--- cabledolphin.py
+++ cabledolphin.py
@@ -3,28 +3,7 @@ from time import sleep
 from threading import Thread as th
 from termcolor import colored,cprint
 os.system("clear")
-httprq=0
-httpsrq=0
-httprp=0
-httpsrp=0
-capture = pyshark.LiveCapture(interface='eth0')
-for packet in capture.sniff_continuously():
-    sleep(0.001)
-    os.system("clear")
-    print("HTTP Request {}\nHTTP Response {}\nHTTPS Request {}\nHTTPS Response {}\n".format(httprq,httprp,httpsrq,httpsrp))
-    try:
-        #print(packet["TCP"].dstport)#.dst)
-        if packet["TCP"].dstport == "80":
-            httprp+=1
-            #print(colored("HTTP Response","cyan"))
-        elif packet["TCP"].dstport == "443":
-            httpsrp+=1
-            #print (colored('HTTPS Response',"blue"))
-        elif packet["TCP"].srcport == "80":
-            httprq+=1
-            #print (colored('HTTP Request',"yellow"))
-        elif packet["TCP"].srcport == "443":
-            httprq+=1
-            #print (colored('HTTPS Request',"red"))
-    except:
-        pass
+
+
+
+print(cabledolphinbrain.HTTP,"\n",cabledolphinbrain.HTTPS)
--- cabledolphinbrain.py
+++ cabledolphinbrain.py
@@ -1,3 +1,26 @@
 import os,sys,pyshark
 from time import sleep
 from termcolor import colored,cprint
+
+HTTP=[0,0]
+HTTPS=[0,0]
+
+def incrsrc(port):
+    if port == "80":
+        HTTP[0]+=1
+    elif port == "443":
+        HTTPS[0]+=1
+def incrdst(port):
+    if port == "80":
+        HTTP[1]+=1
+    elif port =="443":
+        HTTPS[1]+=1
+
+def capture():
+    capture = pyshark.LiveCapture(interface='eth0')
+        for packet in capture.sniff_continuously():
+            try:
+                incrsrc(packet["TCP"].srcport)
+                incrdst(packer["TCP"].dstport)
+            except:
+                pass
